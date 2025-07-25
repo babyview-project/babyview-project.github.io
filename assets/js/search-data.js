44a678558ebf9c9c078fd7f8bd0e9032ff078ea2
JaneYang07 <49352816+JaneYang07@users.noreply.github.com>
2025-07-25 18:06:33 +0000

    Deploying to gh-pages from @ babyview-project/babyview-project.github.io@56d520a9bdd427a311561444ac9aba4bb7eaf1cf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,9 +429,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-quantifying-infants-everyday-experiences-with-objects-in-a-large-corpus-of-egocentric-videos",
+          title: 'Quantifying infants’ everyday experiences with objects in a large corpus of egocentric videos...',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
